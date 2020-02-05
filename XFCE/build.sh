4c406fe0a5c84ee41feff98457153f26c338c6c5
PeterDauwe <bobo5290461@gmail.com>
2020-02-05 10:12:36 +0100

    up

--- XFCE/build.sh
+++ XFCE/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #set -e
-buildFolder="$HOME/Arcoboboxfce-build"
-outFolder="$HOME/Arcoboboxfce-Out"
+buildFolder="$HOME/ArcoBobo-build"
+outFolder="$HOME/ArcoBobo-Out"
 
 echo
 echo "################################################################## "
@@ -88,9 +88,9 @@ echo "Copying files and folder to build folder as root"
 sudo mkdir $buildFolder
 sudo cp -r work/* $buildFolder
 
-sudo chmod 750 ~/Arcoboboxfce-build/archiso/airootfs/etc/sudoers.d
-sudo chmod 750 ~/Arcoboboxfce-build/archiso/airootfs/etc/polkit-1/rules.d
-sudo chgrp polkitd ~/Arcoboboxfce-build/archiso/airootfs/etc/polkit-1/rules.d
+sudo chmod 750 ~/ArcoBobo-build/archiso/airootfs/etc/sudoers.d
+sudo chmod 750 ~/ArcoBobo-build/archiso/airootfs/etc/polkit-1/rules.d
+sudo chgrp polkitd ~/ArcoBobo-build/archiso/airootfs/etc/polkit-1/rules.d
 
 echo "Deleting the work folder if one exists - clean up"
 [ -d work ] && rm -rf work
@@ -108,7 +108,7 @@ yes | sudo pacman -Scc
 
 echo
 echo "################################################################## "
-tput setaf 2;echo "Phase 7 : Building the iso";tput sgr0
+tput setaf 2;echo "Phase 7 : Build ISO";tput sgr0
 echo "################################################################## "
 echo
 
@@ -128,6 +128,5 @@ echo "################################################################## "
 tput setaf 2;echo "Phase 9 : Making sure we start with a clean slate next time";tput sgr0
 echo "################################################################## "
 echo
-#speaker-test -c6 -s1 -f75
-#echo "Deleting the build folder if one exists - takes some time"
-#[ -d $buildFolder ] && sudo rm -rf $buildFolder
+echo "Deleting the build folder if one exists - takes some time"
+[ -d $buildFolder ] && sudo rm -rf $buildFolder

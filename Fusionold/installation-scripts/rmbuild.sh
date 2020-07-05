01aa26176fbf92b2b7dc7881c5a2f58d3ccd1498
PeterDauwe <bobo5290461@gmail.com>
2020-07-05 12:19:41 +0200

    up

---
+++ Fusionold/installation-scripts/rmbuild.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#set -e
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+buildFolder="$HOME/arcobobo-build"
+
+echo
+echo "################################################################## "
+tput setaf 2;echo "Phase 9 : Making sure we start with a clean slate next time";tput sgr0
+echo "################################################################## "
+echo
+echo "Deleting the build folder if one exists - takes some time"
+[ -d $buildFolder ] && sudo rm -rf $buildFolder
